ef9dd395e3499d1fa257eb5c8acebba618d249ae
VicN <vnikolaeff989@gmail.com>
2022-10-18 22:09:43 +0300

    Project: Simple Tic-Tac-Toe. Stage 5/5 init

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,19 +1,15 @@
 package tictactoe
 
 fun main() {
-    val str = readln()
-    val grid = mutableListOf<MutableList<Char>>()
-    for (s in str.chunked(3)) {
-        val list = mutableListOf<Char>()
-        s.forEach { list += it }
-        grid += list
-    }
+    val grid = Array(3) { Array(3) { '_' }.toMutableList() }.toMutableList()
     printGrid(grid)
 
+    var end = false
+    var step = 0
     do {
         val move = mutableListOf<Int>()
         try {
-            readln().split(" ").map { move += it.toInt() - 1 }
+            readln().split(' ').map { move += it.toInt() - 1 }
         } catch (e: NumberFormatException) {
             println("You should enter numbers!")
             continue
@@ -22,13 +18,30 @@ fun main() {
             move[0] !in 0..2 || move[1] !in 0..2 -> println("Coordinates should be from 1 to 3!")
             grid[move[0]][move[1]] != '_' -> println("This cell is occupied! Choose another one!")
             else -> {
-                grid[move[0]][move[1]] = 'X'
-                break
+                step++
+                grid[move[0]][move[1]] = if (step % 2 == 0) 'O' else 'X'
+                if (step >= 5) {
+
+                }
             }
         }
-    } while (true)
+    } while (!end)
 
     printGrid(grid)
+}
+
+fun countingTheGame(grid: MutableList<MutableList<Char>>, state: Char): Boolean {
+    var counter = 0
+    for (i in 0..grid.lastIndex) {
+        for (j in 0..grid[0].lastIndex) {
+            if (grid[i][j] == state) counter++
+        }
+        if (counter == grid.size) {
+            break
+        } else counter = 0
+
+    }
+    return game
 }
 
 fun printGrid(grid: MutableList<MutableList<Char>>) {
